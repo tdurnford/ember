c9ad6c6ce362d83f923e5485e58d5e0a055c6f6b
TJ Durnford <tjdford@gmail.com>
2024-11-15 14:36:27 -0600

    [Chore]: Update functions workflow

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,15 +1,38 @@
 import { Text, View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Index() {
+  const { data, isError, isLoading } = useQuery<
+    {
+      userId: number;
+      id: number;
+      title: string;
+      completed: boolean;
+    }[]
+  >({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      return response.json();
+    },
+  });
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        overflowY: "auto",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {data?.map((todo) => <Text key={todo.id}>{todo.title}</Text>)}
     </View>
   );
 }
